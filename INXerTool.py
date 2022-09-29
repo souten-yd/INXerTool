9b8f0254ed9a5aa9e2b5ef108d01bae3b863befc
souten <k.kenshirou@gmail.com>
2022-09-29 15:35:47 +0900

    株価の表示を選択できるように修正

--- INXerTool.py
+++ INXerTool.py
@@ -2,9 +2,10 @@
 #qpy:quiet
 #qpy:qpyapp
 
-####
+####設定変更箇所###
 INXTokenHoldCount = 1000 #INXToken保有数を記載
-####
+ShowStockPrice = False #True:INX株価表示, False:INX株価非表示 処理時間が変わる。
+##################
 
 import androidhelper
 droid = androidhelper.Android()
@@ -14,6 +15,7 @@ import requests
 import json
 import math
 from bs4 import BeautifulSoup as bs
+
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT"
 
 def getSoup(url):
@@ -28,7 +30,7 @@ def getINXTokenPrice():
     soup = getSoup(url)
     targetElement = soup.find("div",class_="elementor-element elementor-element-907dbcc elementor-widget__width-auto elementor-widget elementor-widget-text-editor")
     INXTokenValue = (targetElement.find("div").text).strip("\n $")
-    return INXTokenValue
+    return float(INXTokenValue)
 
 def getINXTokenHolder():
     url = "https://etherscan.io/dex/uniswapv3/0x4a9353bd25abe95f103e91e27eb63652f2cb7e6a"
@@ -42,25 +44,30 @@ def exchangeRatio():
     res = requests.get(url)
     data = res.json()
     USDJPY = data["rates"]["JPY"]
-    return USDJPY
+    return float(USDJPY)
 
 def getStockPrice(Name:str):
     url = "https://finance.yahoo.com/quote/" + Name
     soup = getSoup(url)
     value = soup.find("fin-streamer", class_="Fw(b) Fz(36px) Mb(-4px) D(ib)").text
-    return value
+    return float(value)
 
-INXTokenPrice = float(getINXTokenPrice())
-INXTokenHolder = getINXTokenHolder()
-INXStockPrice = float(getStockPrice("INXDF"))
-USDJPY = float(exchangeRatio())
-
+USDJPY = exchangeRatio()
+INXTokenPrice = getINXTokenPrice()
 INXTokenJPY = USDJPY * INXTokenPrice
-INXStockJPY = USDJPY * INXStockPrice
-INXTokenSum = int(INXTokenJPY *INXTokenHoldCount)
+INXTokenHolder = getINXTokenHolder()
+if ShowStockPrice:
+    INXStockPrice = getStockPrice("INXDF")
+    INXStockJPY = USDJPY * INXStockPrice
 
-mes = "INXStock: $" + str(round(INXStockPrice,3)) + ", ¥" + str(round(INXStockJPY,1))
-mes = mes + "\n" + "INXToken: $" + str(round(INXTokenPrice,3)) + ", ¥" + str(round(INXTokenJPY,1))
+INXTokenSum = int(INXTokenJPY * INXTokenHoldCount)
+
+if ShowStockPrice:
+    mes = "INXStock: $" + str(round(INXStockPrice,3)) + ", ¥" + str(round(INXStockJPY,1))
+    mes = mes + "\n" + "INXToken: $" + str(round(INXTokenPrice,3)) + ", ¥" + str(round(INXTokenJPY,1))
+else:
+    mes = "INXToken: $" + str(round(INXTokenPrice,3)) + ", ¥" + str(round(INXTokenJPY,1))
+
 mes = mes + "\n" + "INXTokenSum: ¥" + str("{:,d}".format(INXTokenSum))
 mes = mes + "\n" + "INXHodler: " + str(INXTokenHolder)
 mes = mes + "\n" + "USDJPY: " + str(USDJPY)
